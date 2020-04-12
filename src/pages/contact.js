957436df5e69f353022f79f6527a1fff22eccac7
rubenberna <rubenmbernardes@gmail.com>
2020-04-12 17:27:19 +0200

    style

---
+++ src/pages/contact.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Layout from '../components/layout'
+import SEO from '../components/seo'
+
+const Contact = () => {
+
+  return (
+    <Layout>
+      <SEO title="Contact" />
+      <h1>Contact</h1>
+    </Layout>
+  )
+}
+
+export default Contact
