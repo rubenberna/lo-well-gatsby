cc1181ba8ec7875a4140ea7235bd00641909625b
rubenberna <rubenmbernardes@gmail.com>
2020-04-17 20:54:59 +0200

    bug(): webpack error

--- firebaseInit.js
+++ firebaseInit.js
@@ -1,16 +1,16 @@
-const firebase = require('firebase')
-const firebaseConfig = require('./firebaseConfig')
-firebase.initializeApp(firebaseConfig)
+// const firebase = require('firebase')
+// const firebaseConfig = require('./firebaseConfig')
+// firebase.initializeApp(firebaseConfig)
 
-const db = firebase.firestore();
+// const db = firebase.firestore();
 
-// Shortcuts for db collections
-const events = db.collection('events')
-const therapies = db.collection('therapies')
-const therapists = db.collection('therapists')
+// // Shortcuts for db collections
+// const events = db.collection('events')
+// const therapies = db.collection('therapies')
+// const therapists = db.collection('therapists')
 
-module.exports = {
-  events,
-  therapies,
-  therapists
-}
+// module.exports = {
+//   events,
+//   therapies,
+//   therapists
+// }
