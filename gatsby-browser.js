2efcb0e1a2b581a85a2fd6d07536fdd39b69712f
rubenberna <rubenmbernardes@gmail.com>
2020-04-18 07:46:38 +0200

    bug(): webpack error

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,10 +1,10 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import "firebase/auth"
 import "firebase/firestore"
-// import React from 'react'
+import React from 'react'
 
-// import { AuthProvider } from './src/context/auth'
+import { AuthProvider } from './src/context/auth'
 
-// export const wrapRootElement = ({ element }) => (
-//   <AuthProvider>{element}</AuthProvider>
-// )
+export const wrapRootElement = ({ element }) => (
+  <AuthProvider>{element}</AuthProvider>
+)
