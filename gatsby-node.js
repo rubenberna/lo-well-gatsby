e1075659460cf2b517b5fdfc27cacd576ce4a405
rubenberna <rubenmbernardes@gmail.com>
2020-04-18 16:33:57 +0200

    bug(): webpack error in prod

--- gatsby-node.js
+++ gatsby-node.js
@@ -101,35 +101,35 @@ exports.createPages = ({ graphql, actions }) => {
 }
 
 
-exports.onCreateWebpackConfig = ({ stage, loaders, actions }) => {
-  if (stage === "build-html") {
-    actions.setWebpackConfig({
-      module: {
-        rules: [
-          {
-            test: /@firebase/,
-            use: loaders.null(),
-          },
-        ],
-      },
-    })
-  }
-}
-
-// exports.onCreateWebpackConfig = ({ stage, actions, getConfig }) => {
-//   if (stage === 'build-html') {
+// exports.onCreateWebpackConfig = ({ stage, loaders, actions }) => {
+//   if (stage === "build-html") {
 //     actions.setWebpackConfig({
-//       // Don't bundle modules that reference browser globals such as `window` and `IDBIndex` during SSR.
-//       // See: https://github.com/gatsbyjs/gatsby/issues/17725
-//       externals: getConfig().externals.concat(function (_context, request, callback) {
-//         // Exclude bundling firebase* and react-firebase*
-//         // These are instead required at runtime.
-//         if (/^(react-)?firebase(.*)/.test(request)) {
-//           console.log('Excluding bundling of: ' + request);
-//           return callback(null, 'umd ' + request);
-//         }
-//         callback();
-//       }),
-//     });
+//       module: {
+//         rules: [
+//           {
+//             test: /@firebase/,
+//             use: loaders.null(),
+//           },
+//         ],
+//       },
+//     })
 //   }
-// };
+// }
+
+exports.onCreateWebpackConfig = ({ stage, actions, getConfig }) => {
+  if (stage === 'build-html') {
+    actions.setWebpackConfig({
+      // Don't bundle modules that reference browser globals such as `window` and `IDBIndex` during SSR.
+      // See: https://github.com/gatsbyjs/gatsby/issues/17725
+      externals: getConfig().externals.concat(function (_context, request, callback) {
+        // Exclude bundling firebase* and react-firebase*
+        // These are instead required at runtime.
+        if (/^(react-)?firebase(.*)/.test(request)) {
+          console.log('Excluding bundling of: ' + request);
+          return callback(null, 'umd ' + request);
+        }
+        callback();
+      }),
+    });
+  }
+};
